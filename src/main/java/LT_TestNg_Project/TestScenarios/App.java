ffce0ba8f2f24eadbb30b494d7ecee59930cb977
PandharinathLondhe <pandhari.0305@gmail.com>
2024-12-09 10:17:52 +0530

    Selenium code to main branch

---
+++ src/main/java/LT_TestNg_Project/TestScenarios/App.java
@@ -0,0 +1,13 @@
+package LT_TestNg_Project.TestScenarios;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
